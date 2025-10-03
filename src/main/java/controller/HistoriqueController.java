786e03c9686805f435118786352631add16b7459
Ezekiela <ezekielaharisoa825@gmail.com>
2025-10-03 21:57:49 +0300

    premier commit avec structure fait mais pas encore de code

---
+++ src/main/java/controller/HistoriqueController.java
@@ -0,0 +1,17 @@
+
+package main.java.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class HistoriqueController implements Initializable {
+
+   
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
